94860ecbcab6924956d67428741f493bea7448ce
hafizsyawal22-ai <hafizsyawal22@gmail.com>
2026-05-20 11:58:25 +0700

    hapus

--- Assets/Scripts/score databese/StudentScoreData.cs
+++
@@ -1,6 +0,0 @@
-[System.Serializable]
-public class StudentScoreData
-{
-    public string studentName;
-    public int score;
-}
--- Assets/Scripts/script_quiz/SO_QuestionData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "SO_QuestionData", menuName = "Scriptable Objects/SO_QuestionData")]
-public class SO_QuestionData : ScriptableObject
-{
-   public List<QuestionData> questions; 
-}
